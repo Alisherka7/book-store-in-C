773bc28e9840466b8289fa17422882c1463b4048
Alisher Tog'ayev <38793933+Alisherka7@users.noreply.github.com>
2020-02-23 02:53:05 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char txt1[1000];
+    char txt2[1000];
+    char txt3[1000];
+    scanf("%s", txt1);
+    scanf("%s", txt2);
+    scanf("%s", txt3);
+    printf("%s\n",txt1);
+    printf("%s\n",txt2);
+    printf("%s\n",txt3);
+    return 0;
+}
